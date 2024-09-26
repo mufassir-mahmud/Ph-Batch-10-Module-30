9136364d99e673588701677f9d5eb6d3b3a3cb01
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-09-26 10:44:55 +0600

    first commit

---
+++ arrow.js
@@ -0,0 +1,11 @@
+// function add(a,b){
+//     const result = a+b;
+//     return a+b;
+
+// }
+const add = (a,b) => a +b;
+const add2 = (c,d) => c * d;
+const sum = add(5,6);
+const mult = add2(5,5);
+console.log(mult);
+console.log(sum);
